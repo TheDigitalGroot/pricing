30a6cb196f0eee9bc53814eab132ce244a965751
Tenzing Namgyal Wangdi <twangdi@pop-os.localdomain>
2022-01-05 11:11:06 +0100

    environment setup

---
+++ src/test/java/com/octo/commerce/DiscountCalculatorTest.java
@@ -0,0 +1,15 @@
+package com.octo.commerce;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit tests for Discount Calculator
+ */
+public class DiscountCalculatorTest {
+    @Test
+    void checkFixtureSetup() {
+        assertTrue(true);
+    }
+}
