f26f2a7739577fad73e6602a10afd4bd6c568b02
Digital Groot <architecting.design@gmail.com>
2022-01-06 00:05:43 +0100

    writing a failing test

--- src/test/java/com/octo/commerce/PromotionEngineTest.java
+++ src/test/java/com/octo/commerce/PromotionEngineTest.java
@@ -103,4 +103,40 @@ public class PromotionEngineTest {
         Optional<BigDecimal> orderTotal = promotionEngine.calculateOrderTotal(cart);
         Assertions.assertEquals(new BigDecimal(370), orderTotal.get());
     }
+
+    /**
+     * Scenario C: Adding four skus to the cart and getting the total when discounts match
+     */
+    @Test
+    void calculateTotalOrderValueWithMatchingDiscountsforAndComboItems() {
+        SKUItem skuItemA = new SKUItem("A", new BigDecimal(50), 3);
+        SKUItem skuItemB = new SKUItem("B", new BigDecimal(30), 5);
+        SKUItem skuItemC = new SKUItem("C", new BigDecimal(20), 1);
+        SKUItem skuItemD = new SKUItem("D", new BigDecimal(15), 1);
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.add(skuItemA);
+        cart.add(skuItemB);
+        cart.add(skuItemC);
+        cart.add(skuItemD);
+
+        PromotionEngine promotionEngine = new PromotionEngine();
+        Map<String, Integer> discountCriteriaA = Collections.singletonMap("A", 3);
+        BuyXforAmt buyXFixedPriceDiscountA = new BuyXforAmt(discountCriteriaA, RewardType.FiXED_SINGLE, new BigDecimal(130), "FiXED_SINGLE_A");
+        promotionEngine.addDiscount(buyXFixedPriceDiscountA);
+
+
+        Map<String, Integer> discountCriteriaB = Collections.singletonMap("B", 2);
+        BuyXforAmt buyXFixedPriceDiscountB = new BuyXforAmt(discountCriteriaB, RewardType.FiXED_SINGLE, new BigDecimal(45), "FiXED_SINGLE_B");
+        promotionEngine.addDiscount(buyXFixedPriceDiscountB);
+
+        Map<String, Integer> discountCriteriaCD = new HashMap<>();
+        discountCriteriaCD.put("C", 1);
+        discountCriteriaCD.put("D", 1);
+        BuyXandYforAmt buyXandYforAmt = new BuyXandYforAmt(discountCriteriaCD, RewardType.FiXED_COMBO, new BigDecimal(30), "FIXED_COMBINED");
+        promotionEngine.addDiscount(buyXandYforAmt);
+
+        Optional<BigDecimal> orderTotal = promotionEngine.calculateOrderTotal(cart);
+        Assertions.assertEquals(new BigDecimal(280), orderTotal.get());
+    }
 }
